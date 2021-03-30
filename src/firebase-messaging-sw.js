6b0fa6ce9534ab5e691a04e008ff22c62600f214
sulsira <sulsiralumo@gmail.com>
2021-03-30 22:17:34 +0200

    Remove messaging file

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,15 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/8.3.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.3.1/firebase-messaging.js");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8cd38RbSuDKIk8Wy69nq0evwbhBVBzaA",
-  authDomain: "teraal-513cf.firebaseapp.com",
-  databaseURL:
-    "https://teraal-513cf-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "teraal-513cf",
-  storageBucket: "teraal-513cf.appspot.com",
-  messagingSenderId: "770350870608",
-  appId: "1:770350870608:web:1194ad95032e8bc1e26155",
-  measurementId: "G-W14HPTB1MK",
+    apiKey: 'empty',
+    authDomain: 'empty',
+    projectId: 'empty',
+    storageBucket: 'empty',
+    messagingSenderId: 'empty',
+    appId: 'empty',
+    measurementId: 'empty',
 };
 firebase.initializeApp(firebaseConfig);
 
